bd5b26d6fa4b7cdd37776a1cd529395d3efa082f
Jaid <jaid.jsx@gmail.com>
2019-07-02 07:53:09 +0200

    Initialized based on https://github.com/jaid/magina
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

---
+++ src/index.js
@@ -0,0 +1,71 @@
+/** @module ensure-object */
+
+import {isObjectLike} from "lodash"
+
+const isObject = value => {
+  if (Array.isArray(value)) {
+    return false
+  }
+  return isObjectLike(value)
+}
+
+/**
+ * Returns `value`, or `[value]` if `value` is not an array
+ * @param {*} value The value that will be converted to an array
+ * @example
+ * import {ensureArray} from "ensure-object"
+ * ensureArray("abc")
+ * // ["abc"]
+ * @returns {*[]} An array
+ */
+export const ensureArray = value => {
+  if (value === undefined) {
+    return []
+  }
+  if (Array.isArray(value)) {
+    return value
+  } else {
+    return [value]
+  }
+}
+
+/**
+ * Returns `value`, or `{key: value}` if `value` is not an object
+ * @param {*} value The value that will be converted to an array
+ * @param {string} key The key of the object entry that might be created on a new object
+ * @example
+ * import {ensureObject} from "ensure-object"
+ * ensureObject("abc", "importantKey")
+ * // { importantKey: "abc" }
+ * @returns {object} An object
+ */
+export const ensureObject = (value, key) => {
+  if (value === undefined) {
+    return {}
+  }
+  if (value |> isObject) {
+    return value
+  } else {
+    return {
+      [key]: value,
+    }
+  }
+}
+
+/**
+ * Returns `value`, or `value[key]` if `value` is not an object
+ * @param {*} value The value that might be an object
+ * @param {string} key The key whose value gets used on possible unpacking
+ * @example
+ * import {unpackObject} from "ensure-object"
+ * unpackObject({id: "abc"}, "id")
+ * // "abc"
+ * @returns {object} Either the value or an unpacked value
+ */
+export const unpackObject = (value, key) => {
+  if (value |> isObject) {
+    return value[key]
+  } else {
+    return value
+  }
+}
